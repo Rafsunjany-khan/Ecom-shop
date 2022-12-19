720ffd2beaba1edadba8daf6e726feb4b9a90a32
Rafsunjany_khan <rafsunjanykhan21@gmail.com>
2022-12-20 02:08:16 +0600

    add cart and session

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import home, signup, login
 
 urlpatterns = [
-    path('', home.homepage, name='homepage'),
+    path('', home.Homepage.as_view(), name='homepage'),
     path('signup/', signup.SignUp.as_view(), name='signup'),
     path('login/', login.Login.as_view(), name='login'),
 ]
--- mainApp/views/home.py
+++ mainApp/views/home.py
@@ -1,19 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from mainApp.models.product import Product
 from mainApp.models.category import Category
+from django.views import View
 
+class Homepage(View):
+    def post(self, request):
+        product = request.POST.get('product')
+        cart = request.session.get('cart')
+        if cart:
+            quantity = cart.get(product)
+            if quantity:
+                cart[product] = quantity+1
+            else:
+                cart[product] = 1
+        else:
+            cart = {}
+            cart[product] = 1
 
-def homepage(request):
-    products = None
-    category = Category.get_all_category()
-    categoryID = request.GET.get('category')
-    if categoryID:
-        products = Product.get_all_products_by_category_id(categoryID)
-    else:
-        products = Product.get_all_products()
+        request.session['cart'] = cart
+        print(request.session['cart'])
+        return redirect('homepage')
+    def get(self, request):
+        products = None
+        category = Category.get_all_category()
+        categoryID = request.GET.get('category')
+        if categoryID:
+            products = Product.get_all_products_by_category_id(categoryID)
+        else:
+            products = Product.get_all_products()
 
-    data = {}
-    data['products'] = products
-    data['category'] = category
-    print('you are: ', request.session.get('email'))
-    return render(request, 'index.html', data)
+        data = {}
+        data['products'] = products
+        data['category'] = category
+        #print('you are: ', request.session.get('email'))
+        return render(request, 'index.html', data)
